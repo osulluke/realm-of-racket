63f6fae9ebf1abcf4586454ad9314eabfb80ee14
Luke O'Sullivan <osulluke@gmail.com>
2016-10-23 17:35:30 -0700

    Add ch0.rkt code.
    Nothing much here.

---
+++ ch0.rkt
@@ -0,0 +1,8 @@
+#lang racket
+(list 1 2 3 4 5 6 7 8 9 0)
+(list (list 1 3 5 7 9) (list 2 4 6 8 0))
+(list (list 'hello 'world)
+      (list (list 'it 'is) 2063)
+      (list 'and 'we 'love 'racket))
+
+'(sqrt (+ (sqr 3) (sqr 4)))
